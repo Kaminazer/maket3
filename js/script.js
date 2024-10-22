a42350406a2fb54128c3348772ce6f4de8df601d
Kaminazer <nazarkorol3684@gmail.com>
2024-10-22 17:47:54 +0300

    create slider and responsive design, but without fade effect

--- js/script.js
+++ js/script.js
@@ -3,4 +3,14 @@ $(document).ready(function() {
         $('.header_burger, .menu').toggleClass('active');
         $('body').toggleClass('lock');
     })
+    $('.slider').slick({
+        arrows:true,
+        dots:false,
+        adaptiveHeight:true,
+        speed:500,
+        infinite:true,
+        autoplay:true,
+        autoplay:4000,
+        pauseOnFocus:false,
+    });
 });
