7b7f70e2ef025b83bf5c1d6d80e568915b3c9d07
Kaminazer <nazarkorol3684@gmail.com>
2024-10-28 18:26:31 +0200

    made the footer stick to the bottom of the page

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
 $(document).ready(function() {
-    $('.header__burger').click(function(event){
+    $('.header__burger').click(function(event) {
         $('.header__burger').toggleClass('header__burger--active');
         $('.menu').toggleClass('menu--active');
         $('body').toggleClass('lock');
-    }),
-    $('.content__burger').click(function(event){
+    });
+
+    $('.content__burger').click(function(event) {
         $('.content__burger').toggleClass('content__burger--active');
         $('.content-menu').toggleClass('content-menu--active');
         $('body').toggleClass('lock');
-    }),
-    $('.header__phone-icon').click(function(event){
+    });
+
+    $('.header__phone-icon').click(function(event) {
         $('.header__phone-icon').toggleClass('header__phone-icon--active');
         $('.header__phone').toggleClass('header__phone--active');
         $('body').toggleClass('lock');
-    }),
+    });
 
     $('.slider').slick({
         speed: 4000,
-        infinite:true,
+        infinite: true,
         fade: true,
         autoplay: true,
         autoplaySpeed: 4000
-    });    
+    });
 });
